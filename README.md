b8147fcb32306f9763786820d46beb8984066004
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-06-06 17:14:57 -0400

    docs: Upload package to pypi

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DeepNeuro
-TBD - A repository for deep learning models applied to neuroimaging applications.
+A deep learning python package for neuroimaging data.
+
+Created by the Quantitative Tumor Imaging Lab at the Martinos Center (Harvard-MIT Program in Health, Sciences, and Technology / Massachussets General Hospital).
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""DeepNeuro: A deep learning python package for neuroimaging data.
+
+Created by the Quantitative Tumor Imaging Lab at the Martinos Center 
+(Harvard-MIT Program in Health, Sciences, and Technology / Massachussets General Hospital).
+
+"""
+
+DOCLINES = __doc__.split("\n")
+
+import sys
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+if sys.version_info[:2] < (2, 7):
+    raise RuntimeError("Python version 2.7 or greater required.")
+
+setup(
+  name = 'deepneuro',
+  # packages = ['qtim_tools'], # this must be the same as the name above
+  version = '0.1.1',
+  description = DOCLINES[0],
+  packages = find_packages(),
+  author = 'Andrew Beers',
+  author_email = 'abeers@mgh.harvard.edu',
+  url = 'https://github.com/QTIM-Lab/DeepNeuro', # use the URL to the github repo
+  download_url = 'https://github.com/QTIM-Lab/DeepNeuro/tarball/0.1.1',
+  keywords = ['neuroimaging', 'neuroncology', 'neural networks', 'neuroscience', 'neurology', 'deep learning', 'fmri', 'pet', 'mri', 'dce', 'dsc', 'dti', 'machine learning', 'computer vision', 'learning', 'keras', 'theano', 'tensorflow', 'nfiti', 'nrrd', 'dicom'],
+  install_requires=['nibabel'],
+  classifiers = [],
+)
