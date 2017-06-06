36fe4edc19bb9589b8a0ea2f7ceff03543efc2bc
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-06-06 18:27:14 -0400

    docs: Automatic documentation

--- deepneuro/package_test.py
+++ deepneuro/package_test.py
@@ -1,1 +1,25 @@
+"""A test module for automatic documentation purposes
+"""
+
 import deepneuro
+
+def test_function():
+
+    """Some test documentation for automatic documentation.
+
+    Parameters
+    ----------
+    in: str
+        Description.
+
+    Returns
+    -------
+    out: str
+        Description
+        
+    """
+
+    return
+
+if __name__ == '__main__':
+    test_function()
---
+++ docs/source/deepneuro.rst
@@ -0,0 +1,22 @@
+deepneuro package
+=================
+
+Submodules
+----------
+
+deepneuro\.package\_test module
+-------------------------------
+
+.. automodule:: deepneuro.package_test
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: deepneuro
+    :members:
+    :undoc-members:
+    :show-inheritance:
