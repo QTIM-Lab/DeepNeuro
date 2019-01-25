prog: Data fixes, value masking, cluster accuracy

deepneuro/data/data_group.py
@@ -70,8 +70,14 @@ class DataGroup(object):
 
         if self.source == 'hdf5':
             self.preprocessed_case = self.data[index][:][np.newaxis][0]
+
+            # Storing affines needs work. How not to duplicate affines in case
+            # of augmentation, for example?
             if self.data_affines is not None:
-                self.preprocessed_affine = self.data_affines[index]
+                if self.data_affines.shape[0] == 0:
+                    self.preprocessed_affine = None
+                else:
+                    self.preprocessed_affine = self.data_affines[index]
         else:
             self.preprocessed_case, affine = read_image_files(self.preprocessed_case, return_affine=True)
             if affine is not None:
@@ -109,6 +115,11 @@ class DataGroup(object):
 
     # @profile
     def write_to_storage(self):
+
+        # if self.base_case.shape != self.data_storage.shape:
+        #     print(self.base_case.shape, self.data_storage.shape)
+        #     print("""Attempting to write data to HDF5 with incorrect shape. Are you attempting to save different size data inputs to HDF5? DeepNeuro currently only saves equal size inputs/""")
+        #     raise ValueError
 
         if len(self.augmentation_cases) == 1:
             self.data_storage.append(self.base_case)

deepneuro/postprocessing/statistics.py
@@ -76,10 +76,8 @@ def dice_cost_function(input_data, ground_truth):
     
     Parameters
     ----------
-    input_data : TYPE
-        Description
-    ground_truth : TYPE
-        Description
+    input_data, ground_truth : NumPy
+        Arrays to be compared.
     
     Returns
     -------
@@ -106,9 +104,58 @@ def dice_cost_function(input_data, ground_truth):
 
 def accuracy_cost_function(input_data, ground_truth):
 
+    """Summary
+    
+    Parameters
+    ----------
+    input_data, ground_truth : NumPy
+        Arrays to be compared.
+    
+    Returns
+    -------
+    TYPE
+        Description
+    """
+
     return np.sum(input_data == ground_truth)
 
 
-def cluster_accuracy_cost_function(input_data, ground_truth):
+def cluster_accuracy_cost_function(input_data, ground_truth, connectivity=2):
+    
+    """Computes a function to see how many clusters that exist in the ground truth
+        data have overlapping segments in the input data. Note that this does not account
+        for extraneous segmentations in the input data that do not correspond to any
+        clusters in the ground truth data.
+    
+    Parameters
+    ----------
+    input_data, ground_truth : NumPy
+        Arrays to be compared.
+    connectivity : int, optional
+        Description
+    
+    Returns
+    -------
+    float
+        Cluster accuracy metric.
+    """
 
+    if input_data.shape[-1] != 1:
+        raise NotImplementedError('Cluster accuracy not implemented for data with multiple channels.')
 
+    input_data = input_data[0, ..., 0]
+    ground_truth = ground_truth[0, ..., 0]
+
+    overlapping_map = np.logical_and(input_data, ground_truth)
+    connected_components = label(ground_truth, connectivity=connectivity)
+    total_components = np.max(connected_components)
+
+    overlapping_components = 0
+
+    for i in range(1, total_components + 1):
+        individual_component = np.copy(connected_components)
+        individual_component[individual_component != i] == 0
+        if np.sum(np.logical_and(overlapping_map, individual_component.astype(bool))) != 0:
+            overlapping_components += 1
+
+    return overlapping_components / total_components