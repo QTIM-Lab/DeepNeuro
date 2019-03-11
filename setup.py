887b360fb7545bfede361888c3472524e2906946
AndrewBeers <andrew_beers@alumni.brown.edu>
2019-03-11 15:14:19 -0400

    docs: New version setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info[:2] < (2, 7):
 
 setup(
   name='deepneuro',
-  version='0.1.2',
+  version='0.2',
   description=DOCLINES[0],
   packages=find_packages(),
   entry_points= {
@@ -33,7 +33,7 @@ setup(
   author='Andrew Beers',
   author_email='abeers@mgh.harvard.edu',
   url='https://github.com/QTIM-Lab/DeepNeuro',  # use the URL to the github repo
-  download_url='https://github.com/QTIM-Lab/DeepNeuro/tarball/0.1.2',
+  download_url='https://github.com/QTIM-Lab/DeepNeuro/tarball/0.2',
   keywords=['neuroimaging', 'neuroncology', 'neural networks', 'neuroscience', 'neurology', 'deep learning', 'fmri', 'pet', 'mri', 'dce', 'dsc', 'dti', 'machine learning', 'computer vision', 'learning', 'keras', 'theano', 'tensorflow', 'nifti', 'nrrd', 'dicom'],
   install_requires=['tables', 'pydicom', 'pynrrd', 'nibabel', 'pyyaml', 'six', 'imageio', 'matplotlib', 'pydot', 'scipy', 'scikit-image==0.12.3', 'tqdm', 'lycon', 'numpy'],
   classifiers=[],
